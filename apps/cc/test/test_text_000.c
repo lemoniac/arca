5781a6065a5f32c1c9579415e726b711f2204974
lemoniac <aldomon@gmail.com>
2018-11-21 21:07:29 +0100

    [cc] improve text mode test

--- apps/cc/test/test_text_000.c
+++ apps/cc/test/test_text_000.c
@@ -1,9 +1,14 @@
-int *TEXT_RAM = 0x20000000;
-
 int main()
 {
+    int i;
+    int *TEXT_RAM = 0x20000000;
     const int green = 2 << 8;
-    (*TEXT_RAM) = 'A' | green;
+
+    for(i = 0; i < 26; ++i)
+    {
+        int *ptr = TEXT_RAM + (2 * i) + (80 * i);
+        (*ptr) = ('A' + i) | green;
+    }
 
     return 0;
 }
